2b39ac24a64c005bcdac96296be81346f3f9b6e3
Claude Glauser <public@claude-glauser.ch>
2017-10-14 18:02:25 +0200

    first version

--- Rover2Android/app/src/main/java/rover2/rover2android/MainActivity.java
+++ Rover2Android/app/src/main/java/rover2/rover2android/MainActivity.java
@@ -5,11 +5,25 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.WebSocket;
+import okhttp3.WebSocketListener;
+import okio.ByteString;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebSocket socket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +38,106 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+
+        //Button
+        final Button buttonSend = (Button) findViewById(R.id.buttonSend);
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sendText();
+            }
+        });
+
+        final Button buttonStart = (Button) findViewById(R.id.buttonStart);
+        buttonStart.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                startSocketListener();
+            }
+        });
+
+    }
+
+    private void sendCommandToArduino(final String message)
+    {
+        if(message == null || message.equals("Init") || message.equals(""))
+        {
+            return;
+        }
+        TextView tv  = (TextView) findViewById(R.id.editTextAdress);
+        String server = tv.getText().toString();
+
+        OkHttpClient client = new OkHttpClient();
+
+        String url = "http://" + server + message;
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response response = null;
+        try {
+            response = client.newCall(request).execute();
+            String body = response.body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private final class EchoWebSocketListener extends WebSocketListener {
+        private static final int NORMAL_CLOSURE_STATUS = 1000;
+        @Override
+        public void onOpen(WebSocket webSocket, Response response) {
+            webSocket.send("Init");
+        }
+        @Override
+        public void onMessage(WebSocket webSocket, String text) {
+            sendCommandToArduino(text);
+        }
+
+        @Override
+        public void onMessage(WebSocket webSocket, ByteString bytes) {
+            output("Receiving bytes : " + bytes.hex());
+        }
+        @Override
+        public void onClosing(WebSocket webSocket, int code, String reason) {
+            webSocket.close(NORMAL_CLOSURE_STATUS, null);
+            output("Closing : " + code + " / " + reason);
+        }
+        @Override
+        public void onFailure(WebSocket webSocket, Throwable t, Response response) {
+            output("Error : " + t.getMessage());
+        }
+    }
+
+    public void startSocketListener() {
+        OkHttpClient client = new OkHttpClient.Builder()
+                .readTimeout(0,  TimeUnit.MILLISECONDS)
+                .build();
+
+        EchoWebSocketListener listener = new EchoWebSocketListener();
+
+        Request request = new Request.Builder()
+                .url("ws://rover2.azurewebsites.net/ws")
+                .build();
+        this.socket = client.newWebSocket(request, listener);
+
+        client.dispatcher().executorService().shutdown();
+    }
+
+    public void sendText() {
+        TextView tv  = (TextView) this.findViewById(R.id.textViewReceived);
+        String text = tv.getText().toString();
+        this.socket.send(text);
+    }
+
+    private void output(final String output) {
+        final TextView textView = (TextView) findViewById(R.id.textViewReceived);
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                textView.setText(textView.getText().toString() + "\n\n" + output);
             }
         });
     }
